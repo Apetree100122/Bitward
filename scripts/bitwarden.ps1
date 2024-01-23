5012fb84ec516ae3b47c61a1e61962184c7f6c1f
Alexander Petree <apetree1001@email.phoenix.edu>
2024-01-22 19:29:38 -0800

    Update bitwarden.ps1

--- scripts/bitwarden.ps1
+++ scripts/bitwarden.ps1
@@ -1,3 +1,8 @@
-$scriptPath = $MyInvocation.MyCommand.Path
-Invoke-RestMethod -OutFile $scriptPath -Uri "https://go.btwrdn.co/bw-ps"
-Write-Output "We have moved our self-hosted scripts to their own repository (https://github.com/bitwarden/self-host).  Your 'bitwarden.ps1' script has been automatically upgraded. Please run it again."
+$scriptPath =$My Invocation.My Command.Path
+Invoke-Rest Method 
+-OutFile $script Path 
+-Uri "https://go.btwrdn.co/bw-ps" Write-Output "We have moved our self-hosted scripts to their own repository (https://github.com/bitwarden/self-host). 
+Your 'bitwarden.ps1' script has been 
+automatically upgraded. 
+Please
+run it again."
